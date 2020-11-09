b96e3ceb31d9de7e37be99de2bf85a9fcfda57c9
Pete Graham <pete.graham@zerodeposit.com>
2020-11-09 13:20:10 +0000

    Add block clock 1

--- block_clock_1/sketch.js
+++ block_clock_1/sketch.js
@@ -13,11 +13,13 @@ function draw() {
     stroke(255);
     if (frameCount % 60 == 0) {
         timer++;
+        // TODO: replae with max timer variable
         if (timer == 61){
             timer = 0;
             background('black');
+        }else{
+            draw_block(timer);
         }
-        draw_block(timer);
     }
 }
 
@@ -25,7 +27,8 @@ function draw() {
 function draw_block(number) {
     // blocks are zero indexed
     number -= 1;
-    blocks_per_row = 3
+    // TODO: pass in as parameter
+    blocks_per_row = 10
 
     row = int(number / blocks_per_row)
     y = height - unit - (row * unit);
